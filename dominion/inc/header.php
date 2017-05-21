9e09555b07dfba9e198f2d0486513b932f23598e
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-21 14:37:50 -0500

    update css reference

--- dominion/inc/header.php
+++ dominion/inc/header.php
@@ -4,7 +4,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-  <link rel="stylesheet" href="../static/css/main.css">
+  <link rel="stylesheet" href="../main.css">
 </head>
     
 <body>
